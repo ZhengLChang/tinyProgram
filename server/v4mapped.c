5a4865c58de0dbc23ca075ab02b1b891782bce83
zhenghuijie <13007568302@163.com>
2017-06-18 23:36:19 +0800

    增加ipv4映射成ipv6的函数

---
+++ server/v4mapped.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main(void)
+{
+	struct addrinfo hints, *res, *ressave;
+	char str[128];
+
+	memset(&hints, 0, sizeof(hints));
+
+	hints.ai_flags = AI_V4MAPPED;
+	hints.ai_family = AF_INET6;
+	hints.ai_socktype = SOCK_DGRAM;
+
+	if(0 != getaddrinfo("127.0.0.1", NULL, &hints, &res))
+	{
+		printf("getaddrinfo error: %s\n", strerror(errno));
+		return -1;
+	}
+	ressave = res;
+	for(; res != NULL; res = res->ai_next)
+	{
+		printf("\t%s\n", inet_ntop(AF_INET6, (void *)&((struct sockaddr_in6 *)res->ai_addr)->sin6_addr, str, sizeof(str)));
+	}
+	freeaddrinfo(ressave);
+
+	return 0;
+}
