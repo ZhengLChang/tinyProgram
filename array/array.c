e612f50ab0f4c96b164fbb29f5be261ec7b2d245
zhenghuijie <17688721902@163.com>
2017-08-25 16:47:12 +0800

    add arary

--- array/array.c
+++ array/array.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 
-
+struct test
+{
+	int a;
+	int b;
+};
 static void array_append( void **array,
 		unsigned elem_size,
 		unsigned *p_used,
@@ -12,7 +19,7 @@ static void array_append( void **array,
 	if(*p_size == 0)
 	{
 		*p_size += 5;
-		*array = malloc(5, elem_size);
+		*array = malloc(5 * elem_size);
 		assert(*array);
 	}
 	else if(*p_used == *p_size)
@@ -21,6 +28,60 @@ static void array_append( void **array,
 		*array = realloc(*array, elem_size * (*p_size));
 		assert(*array);
 	}
-	memcpy(*array + (*p_used) ++, value, elem_size);
+	memcpy(*array + elem_size * (*p_used) ++, value, elem_size);
 	return;
+}
+
+int main(void)
+{
+	struct test *arr_p = NULL, arr;
+	unsigned i = 1, used = 0, size = 0;
+	arr.a = 1, arr.b = 100;	
+	
+	for(i = 0; i< 100; i++)
+	{
+		array_append((void **)(&arr_p), sizeof(*arr_p), &used, &size, &arr); arr.a++, arr.b += 100;
+	}
+	for(i = 0;i < used; i++)
+	{
+		printf("%d, %d\n", arr_p[i].a, arr_p[i].b);
+	}
+#if 0
+	int *arr = NULL;
+	unsigned i = 1, used = 0, size = 0;
+	array_append((void **)(&arr), sizeof(int), &used, &size, &i);
+	i++;
+	array_append((void **)(&arr), sizeof(int), &used, &size, &i);
+	i++;
+	array_append((void **)(&arr), sizeof(int), &used, &size, &i);
+	i++;
+	array_append((void **)(&arr), sizeof(int), &used, &size, &i);
+	i++;
+	array_append((void **)(&arr), sizeof(int), &used, &size, &i);
+	i++;
+	array_append((void **)(&arr), sizeof(int), &used, &size, &i);
+	i++;
+	array_append((void **)(&arr), sizeof(int), &used, &size, &i);
+	i++;
+	array_append((void **)(&arr), sizeof(int), &used, &size, &i);
+	i++;
+	array_append((void **)(&arr), sizeof(int), &used, &size, &i);
+	i++;
+	array_append((void **)(&arr), sizeof(int), &used, &size, &i);
+	i++;
+	array_append((void **)(&arr), sizeof(int), &used, &size, &i);
+	i++;
+	array_append((void **)(&arr), sizeof(int), &used, &size, &i);
+	i++;
+	array_append((void **)(&arr), sizeof(int), &used, &size, &i);
+	i++;
+	array_append((void **)(&arr), sizeof(int), &used, &size, &i);
+	i++;
+	for(i = 0; i < used; i++)
+	{
+		printf("%d\n", arr[i]);
+	}
+#endif
+	printf("used = %d, size = %d\n", used, size);
+	return 0;
 }
